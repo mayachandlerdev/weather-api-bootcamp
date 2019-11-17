fa27cb80f1112baee0ccf5fea4208f34c3dd2a7a
Maya Chandler <55395525+mayachandlerdev@users.noreply.github.com>
2019-11-17 17:07:59 -0500

    Add files via upload

--- simple weather api/weatherapi.js
+++ simple weather api/weatherapi.js
@@ -11,11 +11,15 @@ btn.addEventListener('click', ()=> {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=6a81e0e11d96218ba821414b56c56e6a`)
   .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
   .then(response => {
-  console.log(response)
+  console.log((response.main.temp * 5/9) +32 - 132)
+  let text = document.createTextNode((response.main.temp * 5/9) + 32 -132)
+  h1 = document.getElementById('look');
+  h1.appendChild(text)
   })
+
   .catch(err => {
       console.log(`error ${err}`)
       alert("sorry, there are no results for your search")
   });
 });
-//ISO 3166-2:US
+
